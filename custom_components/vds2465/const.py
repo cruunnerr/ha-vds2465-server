44413338f635b652ea3c3990b28db27c8b6151ff
crally <crally@crally.de>
2026-01-06 21:35:53 +0100

    * Polling-Intervall is configurable * Devices can be edited in the UI * States/Values of sensors are stored across restarts

--- custom_components/vds2465/const.py
+++ custom_components/vds2465/const.py
@@ -8,6 +8,7 @@ CONF_ENCRYPT = "encrypted"
 CONF_VDS_DEVICE = "vds_device"
 CONF_VDS_AREA = "vds_area"
 CONF_POLLING_INTERVAL = "polling_interval"
+CONF_PERSIST_STATES = "persist_states"
 
 DEFAULT_PORT = 4100
 DEFAULT_POLLING_INTERVAL = 5
